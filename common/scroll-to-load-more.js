744a51ef603efd7330e956cd229a7e77b0c794e3
Vaclav Kunes <kunesv@users.noreply.github.com>
2019-07-29 22:20:08 +0200

    Update scroll-to-load-more.js

--- common/scroll-to-load-more.js
+++ common/scroll-to-load-more.js
@@ -5,7 +5,7 @@ class ScrollToLoadMore {
 
     scroll(e) {
         document.querySelector('.log').textContent = window.scrollY + ' ' + document.documentElement.scrollTop + ' ' + document.body.scrollTop + ' ' + document.documentElement.scrollHeight + ' ' + document.documentElement.clientHeight + ' ' + document.body.scrollHeight + ' ' + (window.scrollY + window.innerHeight >= document.body.scrollHeight) + ' ' + (window.scrollY + document.body.clientHeight) + ' ' + document.body.scrollHeight + ' ' + window.innerHeight + ' ' + screen.height;
-        if (!document.body.classList.contains('loading-more') && window.scrollY + window.innerHeight === document.body.scrollHeight) {
+        if (!document.body.classList.contains('loading-more') && window.scrollY + window.innerHeight >= document.body.scrollHeight) {
             document.body.classList.add('loading-more');
             setTimeout(()=> {
                 Messages.addMessage(new Message('Ahoj ' + new Date()), 'bottom');
