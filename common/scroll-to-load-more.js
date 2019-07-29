5e9186d0013a23e92fcb6869d4230211e69e3c87
Vaclav Kunes <kunesv@users.noreply.github.com>
2019-07-29 22:12:11 +0200

    Update scroll-to-load-more.js

--- common/scroll-to-load-more.js
+++ common/scroll-to-load-more.js
@@ -4,8 +4,8 @@ class ScrollToLoadMore {
     }
 
     scroll(e) {
-        document.querySelector('.log').textContent = window.scrollY + ' ' + document.documentElement.scrollTop + ' ' + document.body.scrollTop + ' ' + document.documentElement.scrollHeight + ' ' + document.documentElement.clientHeight + ' ' + document.body.scrollHeight + ' ' + (window.scrollY + document.body.clientHeight >= document.body.scrollHeight) + ' ' + (window.scrollY + document.body.clientHeight) + ' ' + document.body.scrollHeight + ' ' + window.innerHeight + ' ' + screen.height;
-        if (window.scrollY + document.body.clientHeight >= document.body.scrollHeight) {
+        document.querySelector('.log').textContent = window.scrollY + ' ' + document.documentElement.scrollTop + ' ' + document.body.scrollTop + ' ' + document.documentElement.scrollHeight + ' ' + document.documentElement.clientHeight + ' ' + document.body.scrollHeight + ' ' + (window.scrollY + window.innerHeight >= document.body.scrollHeight) + ' ' + (window.scrollY + document.body.clientHeight) + ' ' + document.body.scrollHeight + ' ' + window.innerHeight + ' ' + screen.height;
+        if (window.scrollY + window.innerHeight >= document.body.scrollHeight) {
             document.body.classList.add('loading-more');
             setTimeout(()=> {
                 Messages.addMessage(new Message('Ahoj ' + new Date()), 'bottom');
